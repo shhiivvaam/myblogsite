b7724074b03c515d349f114f90699104b1837b9c
Shivam Kumar <life.shivam2394@gmail.com>
2024-05-28 15:43:11 +0530

    Added firebase Configs and removed unnecessary files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCJq1Ji2blbRqFMHvoJWgr2_EmYYv4uIpQ",
+    authDomain: "myblogsite-a7026.firebaseapp.com",
+    projectId: "myblogsite-a7026",
+    storageBucket: "myblogsite-a7026.appspot.com",
+    messagingSenderId: "1003653671378",
+    appId: "1:1003653671378:web:c3b7df106b9a5c0b3e400b",
+    measurementId: "G-51KQBNDHNJ"
+});
+
+const fb = firebase;
+
+export default fb;
